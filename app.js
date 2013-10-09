e15dd078254d6fb0f75bf71683cc422080b79cf6
profbass <profbass@gmail.com>
2013-10-08 21:43:24 -0600

    Express Mobile App Admin setup

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var about = require('./routes/about');
+var http = require('http');
+var path = require('path');
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs'); //js templating
+app.set('view engine', 'jade'); //use jade templating
+app.set('view options', {layout: false}); //creates layout file
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(require('less-middleware')({ src: __dirname + '/public' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//setting routs
+app.get('/', routes.index);
+app.get('/about', about.bio);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/about.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET about page.
+ */
+
+exports.bio = function(req, res){
+  res.render('about', { title: 'About Page' });
+};
